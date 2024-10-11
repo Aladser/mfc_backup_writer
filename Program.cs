4e6f4784ecafdc80a021839345e02fb95568a3de
Aladser <Aladser@users.noreply.github.com>
2024-10-11 17:57:39 +0900

    рфкт

--- Program.cs
+++ Program.cs
@@ -6,11 +6,14 @@ namespace ms_word_writer
 {
     internal static class Program
     {
+        // номер носителя
         public static string STORAGE_NUMBER;
+        // место расположения носителя
         public static string STORAGE_PLACE;
+        // содержание резервной копии
         public static string[] COPY_CONTENT_ARR;
+        // работники
         public static string[] WORKERS_ARR;
-
 
         [STAThread]
         static void Main()
